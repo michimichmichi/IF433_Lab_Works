232d12a449ccc963c332d55160ce2e5fcc7d53c9
michi <michelleoliviah16@gmail.com>
2026-02-02 16:16:17 +0700

    week01 : initial setup and procedural code

---
+++ src/oop_114755_MichelleOliviaHerijanto/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_114755_MichelleOliviaHerijanto.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Nama : " + name + ",Nilai:" + score)
+
+}
