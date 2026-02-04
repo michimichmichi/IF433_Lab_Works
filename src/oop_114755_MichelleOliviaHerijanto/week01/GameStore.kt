0f9332345abc64093fb693eba8dd25b7f0264adf
michi <michelleoliviah16@gmail.com>
2026-02-04 21:14:26 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_114755_MichelleOliviaHerijanto/week01/GameStore.kt
+++ src/oop_114755_MichelleOliviaHerijanto/week01/GameStore.kt
@@ -1,8 +1,13 @@
 package oop_114755_MichelleOliviaHerijanto.week01
 
 fun main(){
-    val gameTitle: String? = "Roblox"
+    val gameTitle: String = "Roblox"
     val price : Int = 1000000
+    val newPrice = calculateDiscount(price).toInt()
 
-
+    //tes hasil
+println( newPrice )
 }
+fun calculateDiscount (price: Int) =
+    if (price > 500000)  0.8 * price
+    else 0.9 * price
