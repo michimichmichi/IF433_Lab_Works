503f23868069d50b59384d1dcfd470ed20b0d162
michi <michelleoliviah16@gmail.com>
2026-02-04 20:51:35 +0700

    week01: (tugas1) refactor variables to val and use string templates

--- src/oop_114755_MichelleOliviaHerijanto/week01/CircleCalculator.kt
+++ src/oop_114755_MichelleOliviaHerijanto/week01/CircleCalculator.kt
@@ -1,17 +1,17 @@
 package oop_114755_MichelleOliviaHerijanto.week01
 
 fun main(args: Array<String>) {
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+    val radius = 7.0
+    val pi = 3.14
 
-    var area: Double = pi * radius * radius
+    var area = pi * radius * radius
 
-    println("Radius: " + radius + ", Area: " + area)
+    println("Radius: $radius "  + ", Area: $area" )
 
     checkSize(area)
 }
 
-fun checkSize(area: Double) {
+fun checkSize(area : Double ) {
     if (area > 100) {
         println("This is a Big Circle")
     } else {
