dd2999556524b508877134d8aba90d58e3ed6d05
michi <michelleoliviah16@gmail.com>
2026-02-09 13:54:02 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_114755_MichelleOliviaHerijanto/week02/Main.kt
@@ -0,0 +1,29 @@
+package oop_114755_MichelleOliviaHerijanto.week02
+
+import java.util.Scanner
+
+fun main (){
+    val scanner = Scanner(System.`in`)
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print ("Masukkan NIM (Wajib 5 karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if(nim.length != 5){
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5v karakter!")
+    }else{
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+
+    }
+
+
+}
