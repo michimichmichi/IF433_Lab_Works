4bde429602743ba95b7ac5b3eec8ccd4d3692a29
michi <michelleoliviah16@gmail.com>
2026-02-09 14:02:35 +0700

    week02: add secondary constructor

--- src/oop_114755_MichelleOliviaHerijanto/week02/Student.kt
+++ src/oop_114755_MichelleOliviaHerijanto/week02/Student.kt
@@ -1,13 +1,16 @@
 package oop_114755_MichelleOliviaHerijanto.week02
 
-class Student (val name: String, val nim: String, val major: String){
-    init{
-        if(nim.length != 5) {
+class Student (val name: String, val nim: String, val major: String) {
+    init {
+        if (nim.length != 5) {
             println("Warning: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-        }else{
+        } else {
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
     }
 
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan)")
+    }
 }
