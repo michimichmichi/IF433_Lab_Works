a2ab45ba75131c93e727045f328e82ee02e285c6
michi <michelleoliviah16@gmail.com>
2026-02-16 15:59:54 +0700

    week03: test all encapsulated logic in main

--- src/week03/Main.kt
+++ src/week03/Main.kt
@@ -2,6 +2,16 @@ package week03
 
 fun main(){
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+
+    //tes validasi salary
+    e.salary = -1000 //harusnya print error
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    //2. Test Encapsulation
+    e.increasePerformance()
+    //e.performanceRating = 5
+
+    //3.Test Computed Property
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
