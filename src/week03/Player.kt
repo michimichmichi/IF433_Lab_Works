e7b54856d8eeeef4573f6f9471c5f8c1b6910cca
michi <michelleoliviah16@gmail.com>
2026-02-16 22:13:22 +0700

    week3: (task2) create Player testing in main

---
+++ src/week03/Player.kt
@@ -0,0 +1,28 @@
+package week03
+
+class Player ( var username: String){
+    private var xp: Int = 0
+    val level : Int
+        get() = (xp/ 100) + 1
+
+    fun addXp (amount: Int){
+        var currentlevel = level
+            if (amount <= 0){
+                println("ERROR ")
+                return
+
+            } else {
+                xp += amount
+                println(" Current level is $level")
+        }
+        var newlevel = level
+
+        if(newlevel > currentlevel){
+            println("Level Up! Selamat $username naik ke level $level")
+        }
+
+    }
+
+
+
+}
