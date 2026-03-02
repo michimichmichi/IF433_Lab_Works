b06b220767e8f29a1814c937627cadcf9555e576
michi <michelleoliviah16@gmail.com>
2026-03-02 14:20:40 +0700

    week5: create Dosen class inheriting Pegawai

---
+++ src/oop_114755_MichelleOliviaHerijanto/week5/Dosen.kt
@@ -0,0 +1,11 @@
+package oop_114755_MichelleOliviaHerijanto.week5
+
+class Dosen (nama: String, val nidn: String): Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+
+    fun mengajar(){
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
+}
