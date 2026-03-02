279dfcc8ae55a122da939f0b6f5c5d72a6a8b91e
michi <michelleoliviah16@gmail.com>
2026-03-02 15:15:32 +0700

    week5: (task 1) create MathHelper class with overloading

---
+++ src/oop_114755_MichelleOliviaHerijanto/week5/MathHelper.kt
@@ -0,0 +1,16 @@
+package oop_114755_MichelleOliviaHerijanto.week5
+
+class MathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
