886a619e5d5d5ae4ea70a64cd17a2969c334b971
michi <michelleoliviah16@gmail.com>
2026-03-09 16:24:39 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_114755_MichelleOliviaHerijanto/week06/SmartInterfaces.kt
@@ -0,0 +1,19 @@
+package oop_114755_MichelleOliviaHerijanto.week06
+
+interface SmartDevice {
+    val id : String
+    val name: String
+}
+
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+
+interface Recordable{
+    fun startRecord()
+    fun stopRecord(){
+        println("Perekaman dihentikan dan disimpan ke Cloud")
+    }
+
+}
