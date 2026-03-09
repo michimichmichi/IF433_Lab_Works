5462547ebd736a1e52bb53eea8cb37015af98b79
michi <michelleoliviah16@gmail.com>
2026-03-09 22:39:16 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_114755_MichelleOliviaHerijanto/week06/TaskMain.kt
@@ -0,0 +1,13 @@
+package oop_114755_MichelleOliviaHerijanto.week06
+
+fun main(){
+    val lamp = SmartLamp()
+    lamp.name = "Ruang Tamu"
+
+    val speaker = SmartSpeaker()
+    speaker.name = "Google Nest Dapur"
+
+    val cctv = SmartCCTV()
+    cctv.name = "Ezviz Garasi"
+
+}
