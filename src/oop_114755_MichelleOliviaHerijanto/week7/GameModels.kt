a91f9821ba9f4260c550d987b462ca5893542f98
michi <michelleoliviah16@gmail.com>
2026-03-16 19:52:57 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_114755_MichelleOliviaHerijanto/week7/GameModels.kt
@@ -0,0 +1,10 @@
+package oop_114755_MichelleOliviaHerijanto.week7
+
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(10),
+    EPIC(5),
+    LEGENDARY(1)
+}
