e7ae7c5c647b0f37a2d24789de0d5db07a9ae408
michi <michelleoliviah16@gmail.com>
2026-04-13 21:17:44 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_114755_MichelleOliviaHerijanto/week08/LegacyJavaAPI.java
@@ -0,0 +1,9 @@
+package oop_114755_MichelleOliviaHerijanto.week08;
+
+public class LegacyJavaAPI {
+    //Fungsi ini ditulis dalan java. Kotlin tidak tahu uapakah kembaliannya Nullable
+    public static String fetchServerStatus(){
+        return "SERVER_ONLINE_200_OK";
+    }
+
+}
