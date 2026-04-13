cf6284dc158ead91d32fc112015eba8fb66ea265
michi <michelleoliviah16@gmail.com>
2026-04-13 22:08:41 +0700

    week08:(task) create JavaPaymentService for payment interop

---
+++ src/oop_114755_MichelleOliviaHerijanto/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_114755_MichelleOliviaHerijanto.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){
+        return "TRX-" + productId +"-SUCCESS";
+    }
+}
