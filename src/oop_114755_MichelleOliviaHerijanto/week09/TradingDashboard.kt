8bd317f93f57a00284d4676f543353b6a29a49a5
michi <michelleoliviah16@gmail.com>
2026-04-20 21:15:26 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_114755_MichelleOliviaHerijanto/week09/TradingDashboard.kt
@@ -0,0 +1,14 @@
+package oop_114755_MichelleOliviaHerijanto.week09
+
+fun main(){
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG",10,12.5, "OPEN" )
+        ,TradeLog("ETHUSDT", "SHORT", 5, -3.2, "OPEN" )
+        ,TradeLog("BNBUSDT", "LONG", 20, 25.0, "CLOSED" )
+        ,TradeLog("SQLUSDT", "SHORT", 15, -10.8, "CLOSED" )
+        ,TradeLog("XRPUSDT", "LONG", 3, 5.6, "OPEN" )
+        ,TradeLog("ADAUSDT", "SHORT", 8, -2.4, "CLOSED" )
+
+
+    )
+}
