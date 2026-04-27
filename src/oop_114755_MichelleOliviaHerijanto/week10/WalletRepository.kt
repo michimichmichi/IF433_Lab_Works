c202146e50ea3a2fe11ef718a33c4bb8de8da0b7
michi <michelleoliviah16@gmail.com>
2026-04-27 20:55:36 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_114755_MichelleOliviaHerijanto/week10/WalletRepository.kt
@@ -0,0 +1,14 @@
+package oop_114755_MichelleOliviaHerijanto.week10
+
+class WalletRepository<T> {
+
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
