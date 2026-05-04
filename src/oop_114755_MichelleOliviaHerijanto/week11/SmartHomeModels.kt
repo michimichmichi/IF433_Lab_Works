0450d7f31ef3d3142efe1be63c32c72d4165c6f8
michi <michelleoliviah16@gmail.com>
2026-05-04 18:16:41 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_114755_MichelleOliviaHerijanto/week11/SmartHomeModels.kt
+++ src/oop_114755_MichelleOliviaHerijanto/week11/SmartHomeModels.kt
@@ -1,1 +1,5 @@
 data class SmartDevice(var name: String, var category: String, var isOnline: Boolean = false, var powerLoad: Int = 0)
+
+fun SmartDevice.diagnose(): String {
+    return  "[DIAGNOSTIK] $name | Kategori: $category | Status: ${if (isOnline) "Online" else "Offline"} | Daya: $powerLoad Watt"
+}
