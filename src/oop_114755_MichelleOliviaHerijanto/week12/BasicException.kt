a9b4263b56dcf7050076d6759f45c16fddf1ea1d
Michael Immanuel Herijanto <57061376+michaelimmanuel@users.noreply.github.com>
2026-05-11 20:41:26 +0700

    week12: create basic try-catch-finally

---
+++ src/oop_114755_MichelleOliviaHerijanto/week12/BasicException.kt
@@ -0,0 +1,13 @@
+package oop_114755_MichelleOliviaHerijanto.week12
+
+fun divide(a: Int, b: Int): Int {
+    try{
+        return a / b
+    } catch (e: ArithmeticException) {
+        println("Error: ${e.message}")
+        return -1
+    } finally {
+        println("Division attempt finished")
+    }
+
+}
